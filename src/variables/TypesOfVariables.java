1fbef5d9c03b1cfcbe67e9e181571e77c7c8b590
Mansi-manu <jinnamanasa8@gmail.com>
2025-11-04 23:56:25 +0530

    Types Of Variables

---
+++ src/variables/TypesOfVariables.java
@@ -0,0 +1,22 @@
+package variables;
+
+public class TypesOfVariables {
+	//instance variable
+		String instanceVar;
+		//static variable
+		static float pi=3.65f;
+		//method
+		void add() {
+			int a=10;
+			float b=10+pi;
+			System.out.println(b);
+		}
+		//method
+		void print() {
+	    //local variable
+		String localVar="This is local Variable";
+		System.out.println(localVar);
+		}
+
+
+}
---
+++ src/variables/TypesOfVariablesMain.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class TypesOfVariablesMain {
+
+	public static void main(String[] args) {
+		TypesOfVariables t=new TypesOfVariables();
+		System.out.println(t.instanceVar="This is instance" + "Variable data");
+		t.print();
+		t.add();
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
